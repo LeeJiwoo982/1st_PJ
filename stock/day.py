09beccaaf71ffa8d5dd0d6df3c351ed75fc00896
LeeJiwoo982 <easywoo980902@gmail.com>
2023-09-26 16:56:15 +0900

    네이버삼성주가 일자별 2~4페이지 날짜,주가 데이터 뽑고, csv로 저장

--- stock/day.py
+++ stock/day.py
@@ -1,9 +1,23 @@
 import requests
+import csv
 from bs4 import BeautifulSoup
 
-URL = "https://finance.naver.com/item/sise.nhn?code=005930"
-res = requests.get(URL)
-bs = BeautifulSoup(res.text, "html.parser")
+file = open("stock.csv", mode="w", encoding="utf-8", newline="")
+writer = csv.writer(file)
 
-#일별 시세 사이트 들어가기
-print(bs.select_one("iframe").attrs["src"])
+h = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"}
+URL = "https://finance.naver.com//item/sise_day.naver"
+
+for page in range(2,5):
+
+    params = {'code':'005930', 'page':page}
+    
+    res = requests.get(URL, params = params, headers=h)
+    bs = BeautifulSoup(res.text, "html.parser")
+
+
+    for data in bs.select(".type2 tr") :
+        if len(data.select("td")) == 7 :
+            writer.writerow([data.select("td")[0].text.replace(".","-")
+                             , data.select("td")[1].text.replace(",","")])
+file.close()
